c8c27f14f2fd9fa45c2ccb7e5d17fe49476592d7
Justin Willis <jgw9617@gmail.com>
2016-08-10 14:15:47 -0500

    first commit

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {Component, NgZone} from '@angular/core';
+import {NavController, ToastController, App, AlertController} from 'ionic-angular';
 
 import { ContactPage } from '../contact/contact';
 
@@ -7,11 +7,30 @@ import { ContactPage } from '../contact/contact';
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {
-  
+
+  items: any[];
+  coolbool: boolean;
+
+  constructor(private navCtrl: NavController, private app: App, private alertCtrl: AlertController, private zone: NgZone) {
+    this.items = [];
+  }
+
+  ionViewLoaded() {
+    console.log(Date.now());
+  }
+
+  ionViewDidEnter() {
+    this.coolbool = true;
   }
 
   pushPage() {
     this.navCtrl.push(ContactPage);
+  }
+
+  toggleStep() {
+    console.log('fired');
+    this.zone.run(() => {
+      this.coolbool = false;
+    });
   }
 }
